0c67510cdd3914aa92216944843a475eaada6548
ahmedopeyemikabiru <opeyemi.kabiru@yahoo.com>
2023-06-18 11:57:48 +0100

    first commit

---
+++ src/main/java/com/example/countrycitiesapi/CountryCitiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.countrycitiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCitiesApiApplication.class, args);
+	}
+
+}
